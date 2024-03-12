f7fd8c3b8fb41b1f0d412c2bdc41fcdd1606095e
balajimohan <balajimohan@symbiance.com>
2024-03-12 16:11:13 +0530

    Table

--- src/main/java/Apache/PLS/table/PoiTable.java
+++ src/main/java/Apache/PLS/table/PoiTable.java
@@ -4,17 +4,14 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.xwpf.usermodel.*;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTShd;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTcPr;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.STShd;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.*;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Map;
+import java.util.*;
 
 public class PoiTable {
     public static void main(String[] args) throws IOException {
@@ -26,7 +23,7 @@ public class PoiTable {
         Map<String, String> tableJson1 = mlResponse.getTableJson();
         ObjectMapper objectMapper = new ObjectMapper();
         Map<String, String> tableJson = objectMapper.convertValue(tableJson1, new TypeReference<>() {});
-
+//getParaText(document,mlResponse);
         XWPFTable table = document.createTable(tableJson.size(), 2);
         table.setTableAlignment(TableRowAlign.LEFT);
         table.setWidth("90%");
@@ -44,11 +41,11 @@ public class PoiTable {
             if (row == null) {
                 row = table.createRow();
             }
-            if (rowIndex % 2 == 0) {
-                setRowColor(row, "91afee");
-            } else {
-                setRowColor(row, "bbd5fe");
-            }
+//            if (rowIndex % 2 == 0) {
+//                setRowColor(row, "91afee");
+//            } else {
+//                setRowColor(row, "bbd5fe");
+//            }
 
             XWPFParagraph paragraph1 = row.getCell(0).getParagraphs().get(0);
             String trimmedText1 = StringUtils.normalizeSpace(key);
@@ -56,29 +53,41 @@ public class PoiTable {
             paragraph1.setAlignment(ParagraphAlignment.LEFT);
             paragraph1.setIndentationLeft(100);
             paragraph1.setIndentationRight(100);
-            paragraph1.setSpacingAfter(0);
-            row.getCell(0).setText(trimmedText1);
+            paragraph1.setSpacingBefore(100);
+            XWPFRun run = paragraph1.createRun();
+            run.setBold(true);
+            run.setText(trimmedText1);
+            run.setFontSize(12);
+            run.setFontFamily("Times New Roman");
 
 
-            if (StringUtils.containsIgnoreCase(key, "Date")) {
+            if (StringUtils.containsIgnoreCase(key,"Date of this Report")) {
                 SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
                 String currentDate = dateFormat.format(new Date());
                 XWPFParagraph paragraph = row.getCell(1).getParagraphs().get(0);
                 paragraph.setAlignment(ParagraphAlignment.LEFT);
                 paragraph.setIndentationLeft(100);
                 paragraph.setIndentationRight(100);
-                paragraph.setSpacingAfter(0);
+                paragraph.setSpacingBefore(100);
+                XWPFRun run1 = paragraph.createRun();
+                run1.setText(currentDate);
+                run1.setFontSize(12);
+                run1.setFontFamily("Times New Roman");
 
-                row.getCell(1).setText(currentDate);
+//                row.getCell(1).setText(currentDate);
+
             } else {
                 String trimmedText = StringUtils.normalizeSpace(value);
                 XWPFParagraph paragraph = row.getCell(1).getParagraphs().get(0);
                 paragraph.setAlignment(ParagraphAlignment.LEFT);
                 paragraph.setIndentationLeft(100);
+                paragraph.setSpacingBefore(100);
                 paragraph.setIndentationRight(100);
-                paragraph.setSpacingAfter(0);
-
-                row.getCell(1).setText(trimmedText);
+                XWPFRun run1 = paragraph.createRun();
+                run1.setText(trimmedText);
+                run1.setFontSize(12);
+                run1.setFontFamily("Times New Roman");
+//                row.getCell(1).setText(trimmedText);
             }
             rowIndex++;
         }
@@ -87,6 +96,20 @@ public class PoiTable {
         document.write(out);
         out.close();
     }
+    private static void getParaText(XWPFDocument document, MLResponse mlResponse) {
+        if (Objects.nonNull(mlResponse.getParaText())) {
+            List<String> lines = Arrays.stream( mlResponse.getParaText().split("\n")).toList();
+            lines.forEach(s -> {
+                XWPFParagraph paragraph = document.createParagraph();
+                XWPFRun xwpfRun = paragraph.createRun();
+                xwpfRun.setText(s);
+                xwpfRun.setFontSize(12);
+                xwpfRun.setFontFamily("Times New Roman");
+            });
+
+            }
+        }
+
 
     private static void setRowColor(XWPFTableRow row, String color) {
         for (XWPFTableCell cell : row.getTableCells()) {
